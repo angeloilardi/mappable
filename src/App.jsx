f5d7b04a04464de65b3c65917f68ded1582cc68a
Angelo Ilardi <ange448@gmail.com>
2023-04-11 20:05:17 +0100

    added Map component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-// import Map from './components/Map.jsx'
-import {
-    MapContainer,
-    TileLayer,
-    Marker,
-    Popup,
-    useMap,
-} from 'react-leaflet';
+import Map from './components/Map.jsx'
+
 import "./App.css";
 
 function App() {
-  
+  return <Map />
 }
 
 export default App;
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,10 +1,21 @@
-// import {
-//     MapContainer,
-//     TileLayer,
-//     Marker,
-//     Popup,
-//     useMap,
-// } from 'https://cdn.esm.sh/react-leaflet';
 
-// export default function Map() {
-//     return 
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import "../App.css";
+
+export default function Map() {
+  return (
+    <div div className="map">
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+}
